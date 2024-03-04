ecdc52977b796ba7d96de291ebe8bcb8a332d4c0
Usuario <Usuario@CPD03-M15>
2024-03-04 14:38:35 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppJogoVelha2/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiAppJogoVelha2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
